18e7280ef8797de5bf46232cbf92f715a70f716e
Foorack / Max Faxälv <max@foorack.com>
2022-08-16 00:23:25 +0200

    Fix frontend routing issue when not using routing proxy

--- src/routes/frontend.rs
+++ src/routes/frontend.rs
@@ -3,8 +3,9 @@ use actix_web::{
     dev::{ServiceRequest, ServiceResponse},
     HttpResponse,
 };
+use chrono::format;
 use include_dir::{include_dir, Dir};
-use log::{error, warn};
+use log::{debug, error, warn};
 
 static FRONTEND_PUBLIC_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/frontend/public");
 
@@ -67,7 +68,8 @@ pub async fn route_frontend_static_get(
     let path = path
         .chars()
         .into_iter()
-        .skip(SConfig::sub_path().len())
+        // skip prefix and the next /
+        .skip(SConfig::sub_path().len() + 1)
         .collect::<String>();
 
     // fetch using FRONTEND_PUBLIC_DIR.get_file
@@ -83,16 +85,47 @@ pub async fn route_frontend_static_get(
         }
     };
 
-    let body = match file.contents_utf8() {
-        Some(body) => body,
-        None => {
-            error!("Error reading file {}, no content", path);
-            return Err(api_error_internal_error());
-        }
-    };
+    let body = file.contents();
+    let file_path = file.path().to_str().unwrap();
+    let mime_type = match_mime(file_path);
 
-    Ok(ServiceResponse::new(
-        req.clone(),
-        HttpResponse::Ok().body(body),
-    ))
+    // debug!("Serving file {} with mime type {}", file_path, mime_type);
+
+    // Add content type header
+    let mut response = HttpResponse::build(actix_web::http::StatusCode::OK);
+    response.insert_header((actix_web::http::header::CONTENT_TYPE, mime_type));
+    let response = response.body(body);
+    Ok(ServiceResponse::new(req.clone(), response))
+}
+
+fn match_mime(filename: &str) -> String {
+    let ext = filename.split('.').last().unwrap_or("").to_lowercase();
+    match ext.as_str() {
+        // code
+        "html" => "text/html; charset=utf-8",
+        "css" => "text/css; charset=utf-8",
+        "js" => "application/javascript; charset=utf-8",
+        "map" => "application/json; charset=utf-8", // js.map
+        // images
+        "png" => "image/png",
+        "jpg" => "image/jpeg",
+        "jpeg" => "image/jpeg",
+        "gif" => "image/gif",
+        "svg" => "image/svg+xml",
+        "ico" => "image/x-icon",
+        // fonts
+        "woff" => "font/woff",
+        "woff2" => "font/woff2",
+        "ttf" => "font/ttf",
+        "otf" => "font/otf",
+        "eot" => "application/vnd.ms-fontobject",
+        // other
+        "txt" => "text/plain; charset=utf-8",
+        "csv" => "text/csv; charset=utf-8",
+        "pdf" => "application/pdf",
+        "json" => "application/json; charset=utf-8",
+        "zip" => "application/zip",
+        _ => "application/octet-stream",
+    }
+    .to_string()
 }
