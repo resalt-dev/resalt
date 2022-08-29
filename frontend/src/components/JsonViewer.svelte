c219e8ed81efea929c47da4aff2e8f80a196c24e
Foorack / Max Faxälv <max@foorack.com>
2022-08-29 23:54:17 +0200

    Code cleanup

--- frontend/src/components/JsonViewer.svelte
+++ frontend/src/components/JsonViewer.svelte
@@ -1,66 +1,25 @@
 <script lang="ts">
     import { EditorView, basicSetup } from "codemirror";
-    import {
-        ensureSyntaxTree,
-        foldInside,
-        foldNodeProp,
-        foldAll,
-        foldCode,
-        foldable,
-    } from "@codemirror/language";
-    import { EditorState, EditorSelection } from "@codemirror/state";
+    import { ensureSyntaxTree, foldAll } from "@codemirror/language";
+    import { EditorState } from "@codemirror/state";
     import { json } from "@codemirror/lang-json";
     import { onDestroy, onMount } from "svelte";
     import { theme } from "../stores";
     import { resaltDark } from "./codemirror-resalt-theme-dark";
     import { resaltLight } from "./codemirror-resalt-theme-light";
-    import type {
-        Tree,
-        TreeBuffer,
-        SyntaxNode,
-        SyntaxNodeRef,
-    } from "@lezer/common";
 
     export let data: any;
 
     let editorElement: HTMLElement;
     let cm: EditorView = undefined;
-    let tree: Tree | null = null;
 
     $: {
         if (cm) {
             createJSONView("$ update");
 
-            // Find all array nodes
-            let arrayNodes: SyntaxNode[] = [];
-            tree.iterate({
-                enter: (ref: SyntaxNodeRef) => {
-                    //console.log("enter", node.type.name, node);
-                    if (ref.type.name === "Array") {
-                        console.log("Array", ref.type.name, ref);
-                        arrayNodes.push(ref.node);
-                        foldInside(ref.node);
-                    }
-                },
-                leave: (node) => {
-                    //console.log("leave", node);
-                },
-                from: 0,
-                to: tree.length,
-            });
-
-            console.log("arrayNodes", arrayNodes);
-
             if (!(data instanceof Array)) {
                 foldAll(cm);
             }
-
-            // Convert SyntaxNode's to SelectionRange's
-
-            // Select them
-            cm.dispatch({
-                //selection: EditorSelection.create(
-            });
         }
     }
 
@@ -77,7 +36,7 @@
         });
         cm = new EditorView({ state });
         editorElement.replaceChildren(cm.dom);
-        tree = ensureSyntaxTree(state, state.doc.length, 5000);
+        let tree = ensureSyntaxTree(state, state.doc.length, 5000);
         if (tree !== null) {
             console.log(tree);
         } else {
