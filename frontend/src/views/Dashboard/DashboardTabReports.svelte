c107de3b359ddd08d92762c041dad94df286660c
Foorack / Max Faxälv <max@foorack.com>
2022-11-13 20:14:11 +0100

    Improve notes of non-implemented features

--- frontend/src/views/Dashboard/DashboardTabReports.svelte
+++ frontend/src/views/Dashboard/DashboardTabReports.svelte
@@ -3,4 +3,7 @@
     export let report: string | null;
 </script>
 
-<div />
+<div>
+    Not yet implemented. "Reports" will allow you to create and see Reports,
+    which are collections of graphs displaying a variety of data.
+</div>
--- frontend/src/views/Minions/MinionsTabGroups.svelte
+++ frontend/src/views/Minions/MinionsTabGroups.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
 </script>
 
-Groups
+<div>
+    Not yet implemented. "Groups" will allow you to create and manage Groups,
+    which are collections of Minions. Groups will be specified by a set of
+    filters, and can be used as a target for scheduling and running jobs.
+</div>
