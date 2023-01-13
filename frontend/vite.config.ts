2d918b0e7b208656819f82819fc2fb77537a8689
Foorack / Max Faxälv <max@foorack.com>
2023-01-13 12:56:33 +0100

    Remove redundant whitespaces

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import autoprefixer from 'autoprefixer';
 import sveltePreprocess from 'svelte-preprocess';
 
+const tagsRegex1 = /(>)[\s]*([<{])/g;
+const tagsRegex2 = /({[/:][a-z]+})[\s]*([<{])/g;
+const tagsRegex3 = /({[#:][a-z]+ .+?})[\s]*([<{])/g;
+const tagsRegex4 = /([>}])[\s]+(<|{[/#:][a-z][^}]*})/g;
+const tagsReplace = '$1$2';
+
 const production = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
@@ -29,6 +35,14 @@ export default defineConfig({
 				postcss: {
 					plugins: [autoprefixer()],
 				},
+				// Remove redundant whitespaces that affects the layout
+				// https://github.com/sveltejs/svelte/issues/189#issuecomment-1126375112
+				replace: [
+					[tagsRegex1, tagsReplace],
+					[tagsRegex2, tagsReplace],
+					[tagsRegex3, tagsReplace],
+					[tagsRegex4, tagsReplace]
+				],
 			}),
 			compilerOptions: {
 				// enable run-time checks when not in production
