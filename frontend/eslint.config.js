600fc4e65dea5d7560ef16aa6714949f2fba0c25
Foorack / Max Faxälv <max@foorack.com>
2023-09-18 17:43:21 +0200

    Rewrite eslint config

---
+++ frontend/eslint.config.js
@@ -0,0 +1,50 @@
+// eslint.config.js
+
+import globals from 'globals';
+
+// Imports
+import typescript from '@typescript-eslint'
+import sveltePlugin from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+
+// Plugins
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+
+// Parsers
+import svelteParser from 'svelte-eslint-parser';
+import typescriptParser from '@typescript-eslint/parser';
+
+export default [
+	'eslint:recommended',
+	typescriptPlugin.configs.recommended,
+	sveltePlugin.configs.recommended,
+	prettier,
+	{
+		files: ['src/**/*.ts'],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.es2021,
+				...globals.node,
+			},
+			parser: typescriptParser,
+			parserOptions: {
+				sourceType: 'module',
+				ecmaVersion: 2022,
+				extraFileExtensions: ['.svelte'],
+			},
+		},
+		plugins: {
+			typescript: typescriptPlugin,
+		},
+	},
+	{
+		files: ['src/**/*.svelte'],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: typescriptParser,
+			},
+		},
+	},
+];
