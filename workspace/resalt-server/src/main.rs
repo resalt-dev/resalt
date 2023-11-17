861df3c4ca16a643921f451e0acbd2b454ce7fef
Faxälv, Max <max.faxalv@sony.com>
2023-11-17 16:10:47 +0100

    Rework backend Rust workspace

---
+++ workspace/resalt-server/src/main.rs
@@ -0,0 +1,189 @@
+use std::{
+    error::Error,
+    sync::{Arc, Mutex},
+};
+
+use actix_web::{guard::fn_guard, http::header, middleware::*, web, App, HttpServer};
+use env_logger::{init_from_env, Env};
+use log::info;
+use resalt_config::SConfig;
+use resalt_middleware::ValidateAuth;
+use resalt_routes::*;
+use resalt_salt::{SaltAPI, SaltEventListener, SaltEventListenerStatus};
+use resalt_scheduler::Scheduler;
+use resalt_storage::{StorageCloneWrapper, StorageImpl};
+use resalt_storage_mysql::StorageMySQL;
+use resalt_storage_redis::StorageRedis;
+use tokio::task;
+
+async fn init_db() -> Box<dyn StorageImpl> {
+    let db_type = SConfig::database_type();
+    let db_type = db_type.to_lowercase();
+    let db_type = db_type.as_str();
+    info!("Database type: {}", db_type);
+    match db_type {
+        "files" => {
+            let path = SConfig::database_host();
+            Box::new(
+                resalt_storage_files::StorageFiles::connect(&path)
+                    .unwrap_or_else(|_| panic!("Error connecting to {}", &path)),
+            )
+        }
+        "mysql" => {
+            let database_url = format!(
+                "mysql://{}:{}@{}:{}/{}",
+                SConfig::database_username(),
+                SConfig::database_password(),
+                SConfig::database_host(),
+                SConfig::database_port(),
+                SConfig::database_database()
+            );
+            Box::new(
+                StorageMySQL::connect(&database_url)
+                    .await
+                    .unwrap_or_else(|_| panic!("Error connecting to {}", &database_url)),
+            )
+        }
+        "redis" => {
+            let database_url = format!(
+                "redis://{}:{}@{}:{}/{}",
+                SConfig::database_username(),
+                SConfig::database_password(),
+                SConfig::database_host(),
+                SConfig::database_port(),
+                SConfig::database_database()
+            );
+
+            println!("Connecting to {}", &database_url);
+            Box::new(
+                StorageRedis::connect(&database_url)
+                    .await
+                    .unwrap_or_else(|_| panic!("Error connecting to {}", &database_url)),
+            )
+        }
+        _ => todo!(),
+    }
+}
+
+fn start_salt_websocket_thread(db: Box<dyn StorageImpl>) -> Arc<Mutex<SaltEventListenerStatus>> {
+    let listener_status: Arc<Mutex<SaltEventListenerStatus>> =
+        Arc::new(Mutex::new(SaltEventListenerStatus { connected: false }));
+    let salt_listener_status = listener_status.clone();
+    std::thread::spawn(move || {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let ls = task::LocalSet::new();
+        ls.block_on(&rt, async {
+            // Wait a few seconds before starting SSE, so web server gets time to start
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            let salt_ws = SaltEventListener::new(db.clone(), salt_listener_status);
+            salt_ws.start().await;
+        });
+    });
+    listener_status
+}
+
+fn start_scheduler(db: Box<dyn StorageImpl>) {
+    let mut scheduler = Scheduler::new(StorageCloneWrapper { storage: db });
+    scheduler.register_system_jobs();
+    scheduler.start();
+}
+
+async fn run() -> Result<(), Box<dyn Error>> {
+    init_from_env(Env::new().default_filter_or("Debug"));
+
+    // Database
+    let db: Box<dyn StorageImpl> = init_db().await;
+    let db_clone_wrapper = StorageCloneWrapper {
+        storage: db.clone(),
+    };
+
+    // Salt WebSocket Thread
+    let listener_status = start_salt_websocket_thread(db.clone());
+
+    // Scheduler
+    start_scheduler(db);
+
+    HttpServer::new(move || {
+        // Salt API
+        let salt_api = SaltAPI::new();
+
+        let guard_auth = fn_guard(guard_require_auth);
+
+        App::new()
+            .wrap(DefaultHeaders::new().add((header::X_CONTENT_TYPE_OPTIONS, "nosniff")))
+            .wrap(NormalizePath::trim())
+            .wrap(Logger::default())
+            .service(
+                web::scope("/api")
+                    .app_data(web::Data::new(db_clone_wrapper.clone().storage))
+                    .app_data(web::Data::new(salt_api.clone()))
+                    .app_data(web::Data::new(listener_status.clone()))
+                    .wrap(ValidateAuth::new(
+                        db_clone_wrapper.clone().storage,
+                        salt_api,
+                    ))
+                    .service(route_index_get)
+                    .service(route_config_get)
+                    .service(route_metrics_get)
+                    .service(route_login_post)
+                    .service(route_token_post)
+                    .service(
+                        web::scope("/auth")
+                            .guard(guard_auth)
+                            .service(route_myself_get)
+                            .service(route_status_get)
+                            .service(route_minions_get)
+                            .service(route_minion_get)
+                            .service(route_minion_refresh_post)
+                            .service(route_presets_get)
+                            .service(route_presets_post)
+                            .service(route_preset_get)
+                            .service(route_preset_put)
+                            .service(route_preset_delete)
+                            .service(route_grains_get)
+                            .service(route_jobs_get)
+                            .service(route_jobs_post)
+                            .service(route_job_get)
+                            .service(route_events_get)
+                            .service(route_users_get)
+                            .service(route_users_post)
+                            .service(route_user_get)
+                            .service(route_user_delete)
+                            .service(route_user_password_post)
+                            .service(route_user_permissions_post)
+                            .service(route_user_permissions_delete)
+                            .service(route_keys_get)
+                            .service(route_key_accept_put)
+                            .service(route_key_reject_put)
+                            .service(route_key_delete_delete)
+                            .service(route_permissions_get)
+                            .service(route_permissions_post)
+                            .service(route_permission_get)
+                            .service(route_permission_update)
+                            .service(route_permission_delete)
+                            .service(route_settings_import_post)
+                            .service(route_settings_export_get)
+                            .default_service(route_fallback_404),
+                    ),
+            )
+            // Embed web interface
+            .service(web::scope("").default_service(route_frontend_get))
+    })
+    .bind(("0.0.0.0", SConfig::http_port()))?
+    .run()
+    .await?;
+
+    Ok(())
+}
+
+pub fn start() -> Result<(), Box<dyn Error>> {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(run())
+}
+
+pub fn main() {
+    start().unwrap();
+}
---
+++ workspace/resalt-updater/src/update.rs
@@ -0,0 +1,157 @@
+use std::sync::{Arc, Mutex};
+
+use awc::Client;
+use lazy_static::lazy_static;
+use log::*;
+use toml::Table;
+
+const UPDATE_URL: &str = "https://secure.resalt.dev/Cargo.toml";
+
+#[derive(Debug, Clone)]
+pub struct UpdateInfo {
+    pub version: Option<String>,
+    pub news: Option<Vec<String>>,
+    fetch_time: i64,
+}
+
+lazy_static! {
+    static ref CACHE: Arc<Mutex<UpdateInfo>> = Arc::new(Mutex::new(UpdateInfo {
+        version: None,
+        news: None,
+        fetch_time: 0,
+    }));
+    pub static ref CURRENT_VERSION: String = include_str!("../../../Cargo.toml")
+        .lines()
+        .find(|line| line.starts_with("version = "))
+        .and_then(|line| line.split('=').nth(1))
+        .map(|v| v.trim())
+        .map(|v| v.trim_matches('"'))
+        .unwrap_or("unknown")
+        .to_string();
+}
+
+async fn fetch_remote_info() -> Result<UpdateInfo, String> {
+    // Use awc client
+    let client = Client::new();
+
+    let mut resp = match client.get(UPDATE_URL).send().await {
+        Ok(resp) => resp,
+        Err(e) => return Err(format!("Error checking latest version: {}", e)),
+    };
+    let body = match resp.body().await {
+        Ok(body) => body,
+        Err(e) => {
+            return Err(format!(
+                "Error reading body when checking latest version: {}",
+                e
+            ))
+        }
+    };
+    let body_str = match String::from_utf8(body.to_vec()) {
+        Ok(body_str) => body_str,
+        Err(e) => {
+            return Err(format!(
+                "Error decoding body when checking latest version: {}",
+                e
+            ))
+        }
+    };
+    // Parse TOML
+    let cargo = match body_str.parse::<Table>() {
+        Ok(cargo) => cargo,
+        Err(e) => return Err(format!("Error parsing TOML: {}", e)),
+    };
+
+    // Get workspae
+    let workspace = match cargo.get("workspace") {
+        Some(v) => v,
+        None => return Err("Error parsing TOML: no workspace".to_string()),
+    };
+    // Get package
+    let package = match cargo.get("package") {
+        Some(v) => v,
+        None => return Err("Error parsing TOML: no package".to_string()),
+    };
+    // get version
+    let version: String = match package.get("version") {
+        Some(version) => match version.as_str() {
+            Some(version) => version.to_string(),
+            None => return Err("Error parsing TOML: version is not a string".to_string()),
+        },
+        None => return Err("Error parsing TOML: no version".to_string()),
+    };
+
+    // Get news (workspace.metadata.resalt.news)
+    let metadata = match workspace.get("metadata") {
+        Some(metadata) => metadata,
+        None => return Err("Error parsing TOML: no metadata".to_string()),
+    };
+    let resalt = match metadata.get("resalt") {
+        Some(resalt) => resalt,
+        None => return Err("Error parsing TOML: no \"resalt\" metadata".to_string()),
+    };
+    let news = match resalt.get("news") {
+        Some(news) => news,
+        None => return Err("Error parsing TOML: no \"resalt.news\" metadata".to_string()),
+    };
+    // News is a string array
+    let news = match news.as_array() {
+        Some(news) => news,
+        None => return Err("Error parsing TOML: \"resalt.news\" is not an array".to_string()),
+    };
+    let news: Vec<String> = news
+        .iter()
+        .map(|news| match news.as_str() {
+            Some(news) => news.to_string(),
+            None => "Error parsing TOML: \"resalt.news\" is not an array of strings".to_string(),
+        })
+        .collect();
+
+    if !version.eq(CURRENT_VERSION.as_str()) {
+        warn!("========================================================");
+        warn!("==  You are running an outdated version of Resalt!   ===");
+        warn!("==  Please update to the latest version! Understand, ===");
+        warn!("==  running an older version entails SECURITY risk.  ===");
+        warn!("========================================================");
+        warn!("==  Current version: {}", CURRENT_VERSION.as_str());
+        warn!("==  Latest version: {}", version);
+        warn!("========================================================");
+    }
+
+    Ok(UpdateInfo {
+        version: Some(version),
+        news: Some(news),
+        // current timestamp
+        fetch_time: chrono::Utc::now().timestamp(),
+    })
+}
+
+pub async fn get_update_cache(force: bool) -> UpdateInfo {
+    // Drop CACHE MutexGuard lock inbetween of fetching remote version and setting it in the cache
+    if !force {
+        let update_info = CACHE.lock().unwrap();
+        let current_time = chrono::Utc::now().timestamp();
+        let time_diff = current_time - update_info.fetch_time;
+        // Check if 1 hour + 5 minutes has passed
+        if update_info.version.is_some() && update_info.news.is_some() && time_diff < 3900 {
+            return update_info.clone();
+        }
+    }
+    let update_info = match fetch_remote_info().await {
+        Ok(update_info) => update_info,
+        Err(e) => {
+            error!("Error fetching remote version: {}", e);
+            return UpdateInfo {
+                version: None,
+                news: None,
+                fetch_time: 0,
+            };
+        }
+    };
+    {
+        let mut cache = CACHE.lock().unwrap();
+        cache.version = update_info.version.clone();
+        cache.news = update_info.news.clone();
+    }
+    update_info
+}
