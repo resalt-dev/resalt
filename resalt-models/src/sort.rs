40c9844e093be01daa3713801eea4a1365fce4d5
Foorack / Max Faxälv <max@foorack.com>
2023-12-11 22:14:47 +0100

    Remove debug comment

--- resalt-models/src/sort.rs
+++ resalt-models/src/sort.rs
@@ -2,48 +2,6 @@ use serde::{Deserialize, Serialize};
 
 use crate::{Job, Minion};
 
-/*
-minions.sort_by(|a, b| match sort {
-        "id.asc" => a.id.cmp(&b.id),
-        "id.desc" => b.id.cmp(&a.id),
-        "lastSeen.asc" => a.last_seen.cmp(&b.last_seen),
-        "lastSeen.desc" => b.last_seen.cmp(&a.last_seen),
-        "conformitySuccess.asc" => a
-            .conformity_success
-            .unwrap_or_default()
-            .cmp(&b.conformity_success.unwrap_or_default()),
-        "conformitySuccess.desc" => b
-            .conformity_success
-            .unwrap_or_default()
-            .cmp(&a.conformity_success.unwrap_or_default()),
-        "conformityIncorrect.asc" => a
-            .conformity_incorrect
-            .unwrap_or_default()
-            .cmp(&b.conformity_incorrect.unwrap_or_default()),
-        "conformityIncorrect.desc" => b
-            .conformity_incorrect
-            .unwrap_or_default()
-            .cmp(&a.conformity_incorrect.unwrap_or_default()),
-        "conformityError.asc" => a
-            .conformity_error
-            .unwrap_or_default()
-            .cmp(&b.conformity_error.unwrap_or_default()),
-        "conformityError.desc" => b
-            .conformity_error
-            .unwrap_or_default()
-            .cmp(&a.conformity_error.unwrap_or_default()),
-        "osType.asc" => a
-            .os_type
-            .as_ref()
-            .unwrap_or(&String::from(""))
-            .cmp(b.os_type.as_ref().unwrap_or(&String::from(""))),
-        "osType.desc" => b
-            .os_type
-            .as_ref()
-            .unwrap_or(&String::from(""))
-            .cmp(a.os_type.as_ref().unwrap_or(&String::from(""))),
-        _ => std::cmp::Ordering::Equal,
-    }) */
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
 pub enum MinionSort {
     #[default]
@@ -116,16 +74,6 @@ pub fn sort_minions(minions: &mut [Minion], sort: &MinionSort) {
     })
 }
 
-/*
-"id.asc" => a.id.cmp(&b.id),
-"id.desc" => b.id.cmp(&a.id),
-"timestamp.asc" => a.timestamp.cmp(&b.timestamp),
-"timestamp.desc" => b.timestamp.cmp(&a.timestamp),
-"jid.asc" => a.jid.cmp(&b.jid),
-"jid.desc" => b.jid.cmp(&a.jid),
-"user.asc" => a.user.cmp(&b.user),
-"user.desc" => b.user.cmp(&a.user),
-_ => std::cmp::Ordering::Equal, */
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
 pub enum JobSort {
     #[default]
